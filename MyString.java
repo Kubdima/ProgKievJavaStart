b18a8a7914a32f5691bdc0f34d7b6f17e83fb0d1
Kubdima <37818325+Kubdima@users.noreply.github.com>
2018-04-02 10:31:46 +0300

    JavaStart.Lesson2

---
+++ MyString.java
@@ -0,0 +1,10 @@
+public class MyString {
+    public static void main(String[] args){
+	String s1="Рыжий";
+	String s2="Петруха";
+	String s3="Кот";
+	System.out.println(s1 + s3);
+	System.out.println(s3 + (s2 + s3));
+	System.out.println(s1 + s2 + s3);
+    }
+}
